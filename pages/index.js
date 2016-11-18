9e0a1e4d2141080a014ee51f06729afcee5e0183
Lachlan Campbell <lachlan.campbell@icloud.com>
2016-11-18 17:20:16 -0500

    add index page

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import Head from 'next/head'
+
+export default () => (
+  <div>
+    <Head>
+      <title>Call to Action – Call your representatives in Congress</title>
+      <link rel='stylesheet' href='https://unpkg.com/basscss-basic@1.0.0/index.css' />
+    </Head>
+    <h1>Call to Action</h1>
+  </div>
+)
