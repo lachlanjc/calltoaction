5fb0e0c16bcbf3ea8ab44acf8cd4e91d67db3ff5
Lachlan Campbell <lachlan.campbell@icloud.com>
2016-11-19 16:57:49 -0500

    simplify header

--- components/Header.js
+++ components/Header.js
@@ -2,33 +2,22 @@
 import React from 'react'
 import css from 'next/css'
 
-import { colors } from '../styles'
-
 const Header = () => (
   <header className={css(cx.root)}>
-    <div className={css(cx.container)}>
-      <h1 className={css(cx.text)}>Call to Action</h1>
-      <p className={css(cx.text)}>
-        Call your representatives in Congress.
-      </p>
-    </div>
+    <h1 className={css(cx.header)}>Call to Action</h1>
+    <p>Call your representatives in Congress.</p>
   </header>
 )
 
 const cx = {
   root: {
-    paddingTop: 64,
-    paddingBottom: 64,
-    backgroundColor: colors.red
+    textAlign: 'center',
+    marginBottom: 48
   },
-  container: {
-    maxWidth: '48rem',
-    margin: 'auto'
-  },
-  text: {
-    color: colors.white,
-    marginTop: 0,
-    marginBottom: 16
+  header: {
+    textTransform: 'uppercase',
+    letterSpacing: '.1em',
+    fontSize: '2.5rem'
   }
 }
 
