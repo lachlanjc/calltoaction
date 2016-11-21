826600f6c94b62ec4353c04737e12de2f768f2f6
Lachlan Campbell <lachlan.campbell@icloud.com>
2016-11-20 19:39:11 -0500

    standard

--- data/upload-s3.js
+++ data/upload-s3.js
@@ -1,4 +1,5 @@
-const fs = require('fs');
+
+const fs = require('fs')
 const AWS = require('aws-sdk')
 const s3 = new AWS.S3()
 const bucketName = 'usecalltoaction'
@@ -10,32 +11,32 @@ console.log(`Files Length: ${filesLength}`)
 
 const baseCallToActionBucketParams = {
   Bucket: bucketName,
-  ACL: 'public-read',
+  ACL: 'public-read'
 }
 
 for (let i = 0; i < filesLength; i++) {
-  const fileNum = i + 1;
-  console.log(`File #${fileNum} of ${filesLength}`);
+  const fileNum = i + 1
+  console.log(`File #${fileNum} of ${filesLength}`)
 
-  const fileName = files[i];
-  const fileContent = fs.readFileSync(`./dist/${fileName}`, 'utf8');
+  const fileName = files[i]
+  const fileContent = fs.readFileSync(`./dist/${fileName}`, 'utf8')
 
   const fileUploadParams = Object.assign({}, baseCallToActionBucketParams, {
     Key: fileName,
-    Body: fileContent,
-  });
+    Body: fileContent
+  })
 
-  console.log(fileUploadParams);
+  console.log(fileUploadParams)
 
-  s3.putObject(fileUploadParams, function(err, data) {
+  s3.putObject(fileUploadParams, (err, data) => {
     if (err) {
-      console.log(`${fileName} failed to be uploaded to S3.`);
-      console.error(err, err.stack);
+      console.log(`${fileName} failed to be uploaded to S3.`)
+      console.error(err, err.stack)
     } else {
-      console.log(`${fileName} successfully uploaded to S3.`);
-      console.log(data);
+      console.log(`${fileName} successfully uploaded to S3.`)
+      console.log(data)
     }
-  });
+  })
 }
 
 // NOTE! When running this for a fresh deploy, invalidate the CloudFront
