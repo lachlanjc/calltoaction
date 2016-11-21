f82bcd92fa8e7e4c1f9390aa6d2dfbb8c9f6e92d
Lachlan Campbell <lachlan.campbell@icloud.com>
2016-11-20 19:18:52 -0500

    switch to purple

--- theme.js
+++ theme.js
@@ -2,8 +2,9 @@
 import {
   red500, red700,
   blue500,
+  purple500, purple700,
   grey100, grey300, grey400, grey500,
-  white, darkBlack, fullBlack,
+  white, darkBlack, fullBlack
 } from 'material-ui/styles/colors'
 import { fade } from 'material-ui/utils/colorManipulator'
 import spacing from 'material-ui/styles/spacing'
@@ -12,10 +13,10 @@ const theme = {
   spacing: spacing,
   fontFamily: 'inherit',
   palette: {
-    primary1Color: blue500,
-    primary2Color: red700,
+    primary1Color: purple500,
+    primary2Color: purple700,
     primary3Color: grey400,
-    accent1Color: red500,
+    accent1Color: blue500,
     accent2Color: grey100,
     accent3Color: grey500,
     textColor: darkBlack,
